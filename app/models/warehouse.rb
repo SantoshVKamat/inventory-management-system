65bfad93c8e5dc33d3cccf7ca02aca80651422b1
Santosh Kamat <kamatsantosh7194@gmail.com>
2020-11-17 19:04:15 +0530

    Add a new warehouse & set the default count

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,4 +1,13 @@
 class Warehouse < ApplicationRecord
 	has_many :inventories
 	has_many :products, through: :inventories
+
+	after_create :create_default_inventory
+
+	private
+	def create_default_inventory
+		Product.all.each do|product|
+			Inventory.create(product_id: product.id, warehouse_id: self.id, item_count: 0, low_item_threshold:10 )
+		end
+	end
 end
